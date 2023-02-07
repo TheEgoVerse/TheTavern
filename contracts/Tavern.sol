e2c1c043e1428d13735b03096ebb8de4b5843f4d
Alva <srikanthjalva@gmail.com>
2023-02-07 20:24:09 +0530

    multiple nft boost implemented

--- contracts/Tavern.sol
+++ contracts/Tavern.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 interface IRewardToken is IERC20 {
     function mint(address to, uint256 amount) external;
@@ -22,21 +23,9 @@ interface AEPNFT is IERC721 {
     function tokenTierIndex(uint256 id) external view returns (uint256, TIER);
 }
 
-contract Tavern is Ownable, Pausable {
-    IRewardToken public preyContract;
-    AEPNFT public nft;
-
-    uint256 public totalBoostedNFTs;
-    uint256 public totalBoosts;
-    uint256 public stakingStartTime;
-    address public OwnerAddress;
-    uint256 public constant stakingTime = 2 minutes;
-    uint256 public constant MAX_BOOST_COUNT = 5;
-    uint256[] public rewards = [2500e15, 3075e15, 4200e15];
-    uint256[] public MAXREWARD_PER_BOOST = [12500e15, 15375e15, 21000e15];
-
-    struct StakedNFT {
-        uint256 tokenStakingCoolDown;
+contract Tavern is Ownable, Pausable, ReentrancyGuard {
+    struct BoostedNFT {
+        uint256 tokenBoostingCoolDown;
         uint256 rewardEarnedInBoost;
         uint256 rewardReleasedInBoost;
         uint256 totalBoosts;
@@ -44,147 +33,165 @@ contract Tavern is Ownable, Pausable {
         uint256 totalRewardReleased;
     }
 
-    constructor(AEPNFT _nft, IRewardToken _preyContract, address ownerAddress) {
+    IRewardToken public preyContract;
+    AEPNFT public nft;
+
+    uint256 public totalBoostedNFTs;
+    uint256 public totalBoosts;
+    uint256 public boostStartTime;
+    address public teamAddress;
+    uint256 public constant MAX_BOOST_COUNT = 5;
+    uint256[] public rewards = [2500e15, 3075e15, 4200e15];
+    uint256[] public MAXREWARD_PER_BOOST = [12500e15, 15375e15, 21000e15];
+    uint256 public boostInterval = 24 hours;
+    mapping(uint256 => BoostedNFT) public boostedNFTs;
+    bool public initialised;
+
+    constructor(AEPNFT _nft, IRewardToken _preyContract, address _address) {
         nft = _nft;
         preyContract = _preyContract;
-        OwnerAddress = ownerAddress;
+        teamAddress = _address;
     }
 
-    mapping(uint256 => StakedNFT) public stakedNFTs;
+    event Boosted(address indexed owner, uint256 indexed tokenId, uint256 indexed boostCount);
 
-    bool public tokensClaimable;
-    bool initialised;
+    event RewardPaid(address indexed user, uint256 indexed reward);
 
-    function initStaking() public onlyOwner {
+    event PausedStatusUpdated(bool status);
+
+    function initBoosting() public onlyOwner {
         require(!initialised, "Already initialised");
-        stakingStartTime = block.timestamp;
+        boostStartTime = block.timestamp;
         initialised = true;
-    }
-
-    function setTokensClaimable(bool _enabled) public onlyOwner {
-        tokensClaimable = _enabled;
     }
 
     function pause() public onlyOwner {
         _pause();
+        emit PausedStatusUpdated(true);
     }
 
     function unpause() public onlyOwner {
         _unpause();
+        emit PausedStatusUpdated(false);
     }
 
-    function boost(uint256 tokenId, uint256 amount) public whenNotPaused {
-        _boost(tokenId, amount);
+    function boost(uint256 tokenId) public whenNotPaused nonReentrant {
+        _boost(tokenId);
     }
 
-    // function boostBatch(uint256[] memory tokenIds) public whenNotPaused {
-    //     for (uint256 i = 0; i < tokenIds.length; i++) {
-    //         _stake(tokenIds[i]);
-    //     }
-    // }
+    function boostBatch(uint256[] memory tokenIds) public whenNotPaused nonReentrant {
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            _boost(tokenIds[i]);
+        }
+    }
 
-    function _boost(uint256 _tokenId, uint256 _amount) internal {
-        require(initialised, "Staking System: the staking has not started");
-        require(nft.ownerOf(_tokenId) == msg.sender, "user must be the owner of the token");
-        StakedNFT storage stakedNFT = stakedNFTs[_tokenId];
-        require(stakedNFT.totalBoosts < 5, "Max Boosts Reached");
-        if (stakedNFT.tokenStakingCoolDown > 0) {
+    function _boost(uint256 _tokenId) internal {
+        require(initialised, "Boosting System: the boosting has not started");
+        require(nft.ownerOf(_tokenId) == msg.sender, "User must be the owner of the token");
+        BoostedNFT storage boostedNFT = boostedNFTs[_tokenId];
+        require(boostedNFT.totalBoosts < 5, "Max Boosts Reached");
+        if (boostedNFT.tokenBoostingCoolDown > 0) {
             require(
-                stakedNFT.tokenStakingCoolDown + 5 * stakingTime < block.timestamp,
+                boostedNFT.tokenBoostingCoolDown + 5 * boostInterval < block.timestamp,
                 "Boost is Already Active"
             );
+            uint256[] memory tokenList = new uint[](1);
+            tokenList[0] = _tokenId;
+            claimReward(tokenList);
         }
-        require(_amount >= 9_000_000_000_000_000_000, "Invalid Amount");
-        preyContract.burn(msg.sender, 9000000000000000000); // Burn 4.5 $PREY Tokens
-        preyContract.mint(OwnerAddress, 4500000000000000000); // Transfer 4.5 $PREY Tokens
-        claimReward(_tokenId);
-        stakedNFT.totalRewardEarned += stakedNFT.rewardEarnedInBoost;
-        stakedNFT.totalRewardReleased += stakedNFT.rewardReleasedInBoost;
-        stakedNFT.rewardEarnedInBoost = 0;
-        stakedNFT.rewardReleasedInBoost = 0;
-        stakedNFT.tokenStakingCoolDown = block.timestamp;
-        stakedNFT.totalBoosts = stakedNFT.totalBoosts + 1;
-        totalBoostedNFTs = stakedNFT.totalBoosts == 1 ? totalBoostedNFTs + 1 : totalBoostedNFTs;
+        preyContract.burn(msg.sender, 9000000000000000000);
+        preyContract.mint(teamAddress, 4500000000000000000);
+        boostedNFT.totalRewardEarned += boostedNFT.rewardEarnedInBoost;
+        boostedNFT.totalRewardReleased += boostedNFT.rewardReleasedInBoost;
+        boostedNFT.rewardEarnedInBoost = 0;
+        boostedNFT.rewardReleasedInBoost = 0;
+        boostedNFT.tokenBoostingCoolDown = block.timestamp;
+        boostedNFT.totalBoosts = boostedNFT.totalBoosts + 1;
+        totalBoostedNFTs = boostedNFT.totalBoosts == 1 ? totalBoostedNFTs + 1 : totalBoostedNFTs;
         totalBoosts = totalBoosts + 1;
+        emit Boosted(msg.sender, _tokenId, boostedNFT.totalBoosts);
+    }
+
+    function calculateReward(uint256[] memory _tokenIds) public view returns (uint256) {
+        uint256 claimableReward = 0;
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            BoostedNFT storage boostedNFT = boostedNFTs[_tokenIds[i]];
+            if (
+                boostedNFT.tokenBoostingCoolDown < block.timestamp + boostInterval &&
+                boostedNFT.tokenBoostingCoolDown > 0
+            ) {
+                (, AEPNFT.TIER tokenTier) = getTokenTierIndex(_tokenIds[i]);
+                uint256 tierIndex = uint256(tokenTier);
+                uint256 tierReward = rewards[tierIndex]; // 2.5 Token
+                uint256 maxTierReward = MAXREWARD_PER_BOOST[tierIndex];
+                uint256 totalRewardEarned = 0;
+
+                uint256 boostedDays = ((block.timestamp - uint(boostedNFT.tokenBoostingCoolDown))) /
+                    boostInterval;
+                if (tierReward * boostedDays >= maxTierReward) {
+                    totalRewardEarned = maxTierReward;
+                } else {
+                    totalRewardEarned = tierReward * boostedDays;
+                }
+                claimableReward += totalRewardEarned - boostedNFT.rewardReleasedInBoost;
+            }
+        }
+        return claimableReward;
+    }
+
+    function _updateReward(uint256[] memory _tokenIds) internal {
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            BoostedNFT storage boostedNFT = boostedNFTs[_tokenIds[i]];
+            if (
+                boostedNFT.tokenBoostingCoolDown < block.timestamp + boostInterval &&
+                boostedNFT.tokenBoostingCoolDown > 0
+            ) {
+                (, AEPNFT.TIER tokenTier) = getTokenTierIndex(_tokenIds[i]);
+                uint256 tierIndex = uint256(tokenTier);
+                uint256 tierReward = rewards[tierIndex];
+                uint256 maxTierReward = MAXREWARD_PER_BOOST[tierIndex];
+
+                uint256 boostedDays = ((block.timestamp - uint(boostedNFT.tokenBoostingCoolDown))) /
+                    boostInterval;
+                if (tierReward * boostedDays >= maxTierReward) {
+                    boostedNFT.rewardEarnedInBoost = maxTierReward;
+                } else {
+                    boostedNFT.rewardEarnedInBoost = tierReward * boostedDays;
+                }
+            }
+        }
+    }
+
+    function claimReward(uint256[] memory _tokenIds) public whenNotPaused {
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            require(
+                nft.ownerOf(_tokenIds[i]) == msg.sender,
+                "You can only claim rewards for NFTs you own!"
+            );
+        }
+
+        _updateReward(_tokenIds);
+
+        uint256 reward = 0;
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            BoostedNFT storage boostedNFT = boostedNFTs[_tokenIds[i]];
+            reward += boostedNFT.rewardEarnedInBoost - boostedNFT.rewardReleasedInBoost;
+            boostedNFT.rewardReleasedInBoost = boostedNFT.rewardEarnedInBoost;
+        }
+
+        if (reward > 0) {
+            preyContract.mint(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function updateBoostInterval(uint256 hoursCount) external onlyOwner {
+        boostInterval = hoursCount * 1 hours;
     }
 
     function getTokenTierIndex(
         uint256 _id
     ) public view returns (uint256 tokenIndex, AEPNFT.TIER tokenTier) {
         return (nft.tokenTierIndex(_id));
-    }
-
-    function calculateReward(uint256 _tokenId) public view returns (uint256) {
-        uint256 claimableReward = 0;
-        // for (uint256 i = 0; i < _tokenIds.length; i++) {
-        StakedNFT storage stakedNFT = stakedNFTs[_tokenId];
-        if (
-            stakedNFT.tokenStakingCoolDown < block.timestamp + stakingTime &&
-            stakedNFT.tokenStakingCoolDown > 0
-        ) {
-            (, AEPNFT.TIER tokenTier) = getTokenTierIndex(_tokenId);
-            uint256 tierIndex = uint256(tokenTier);
-            uint256 tierReward = rewards[tierIndex]; // 2.5 Token
-            uint256 maxTierReward = MAXREWARD_PER_BOOST[tierIndex];
-            uint256 totalRewardEarned = 0;
-
-            uint256 stakedDays = ((block.timestamp - uint(stakedNFT.tokenStakingCoolDown))) /
-                stakingTime; // Total Days Staked
-            // stakedDays = stakedDays < BOOST_DURATION ? stakedDays : BOOST_DURATION; // // Needs improvement
-            if (tierReward * stakedDays >= maxTierReward) {
-                totalRewardEarned = maxTierReward;
-            } else {
-                totalRewardEarned = tierReward * stakedDays;
-            }
-            claimableReward += totalRewardEarned - stakedNFT.rewardReleasedInBoost;
-        }
-        // }
-        return claimableReward;
-    }
-
-    function _updateReward(uint256 _tokenId) internal {
-        StakedNFT storage stakedNFT = stakedNFTs[_tokenId];
-        if (
-            stakedNFT.tokenStakingCoolDown < block.timestamp + stakingTime &&
-            stakedNFT.tokenStakingCoolDown > 0
-        ) {
-            (, AEPNFT.TIER tokenTier) = getTokenTierIndex(_tokenId);
-            uint256 tierIndex = uint256(tokenTier);
-            uint256 tierReward = rewards[tierIndex];
-            uint256 maxTierReward = MAXREWARD_PER_BOOST[tierIndex];
-
-            uint256 stakedDays = ((block.timestamp - uint(stakedNFT.tokenStakingCoolDown))) /
-                stakingTime;
-            if (tierReward * stakedDays >= maxTierReward) {
-                stakedNFT.rewardEarnedInBoost = maxTierReward;
-            } else {
-                stakedNFT.rewardEarnedInBoost = tierReward * stakedDays;
-            }
-        }
-    }
-
-    function claimReward(uint256 _tokenId) public whenNotPaused {
-        require(tokensClaimable == true, "Tokens cannnot be claimed yet");
-
-        // for (uint256 i = 0; i < _tokenIds.length; i++) {
-        require(
-            nft.ownerOf(_tokenId) == msg.sender,
-            "You can only claim rewards for NFTs you own!"
-        );
-        // }
-
-        _updateReward(_tokenId);
-
-        uint256 reward = 0;
-        // for (uint256 i = 0; i < _tokenIds.length; i++) {
-        StakedNFT storage stakedNFT = stakedNFTs[_tokenId];
-        reward += stakedNFT.rewardEarnedInBoost - stakedNFT.rewardReleasedInBoost;
-        stakedNFT.rewardReleasedInBoost = stakedNFT.rewardEarnedInBoost;
-        // stakedNFT.balance = 0;
-        // }
-
-        if (reward > 0) {
-            preyContract.mint(msg.sender, reward);
-        }
     }
 }
